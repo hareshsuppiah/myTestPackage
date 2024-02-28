0c58e098480326dda06605cbee105963ad945588
Haresh Suppiah <haresh@humanperformance.sg>
2024-02-29 09:54:07 +1100

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Sports Teams Performance Data
+#'
+#' This dataset contains information about the performance of various sports teams, including their number of wins and losses.
+#'
+#' @format ## `data`
+#' A data frame with 4 rows and 4 columns:
+#' \describe{
+#'   \item{X}{An integer indicating the row number.}
+#'   \item{team}{The name of the sports team.}
+#'   \item{wins}{The number of games the team has won.}
+#'   \item{losses}{The number of games the team has lost.}
+#' }
+#' @source Simulated data
+"data"
